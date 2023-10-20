ab7f135450f93ebd3594bce67a58d0a421b7eef8
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-10-20 13:00:52 -0300

    changed logo

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,6 +1,14 @@
+import Logo from "../extensions/logo-cra-lowres.png";
+
 export default {
   config: {
     locales: ["es"],
+    auth: {
+      logo: Logo,
+    },
+    menu: {
+      logo: Logo,
+    },
   },
   bootstrap(app) {
     console.log(app);
